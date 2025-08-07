2cefbd4c9fe9c1948d151f6fb32c1b44da0af745
RAVOTINA Kenny Judickael <judickaelkenny@gmail.com>
2025-08-07 18:19:04 +0300

    add lucide-react for wallet icon and update balance display

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Wallet } from 'lucide-react'
 import { useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
 import api from './api'
@@ -29,9 +30,42 @@ export default function Home() {
     getTransactions()
   }, [])
 
+  const amounts = transactions.map((t) => Number(t.amount) || 0)
+  const balance = amounts.reduce((acc, item) => acc + item, 0) || 0
+  const income =
+    amounts.filter((a) => a > 0).reduce((acc, item) => acc + item, 0) || 0
+  const expense =
+    amounts.filter((a) => a < 0).reduce((acc, item) => acc + item, 0) || 0
+  const ratio =
+    income > 0 ? Math.min((Math.abs(expense) / income) * 100, 100) : 0
+
+  const formatDate = (dateString: string) => {
+    const d = new Date(dateString)
+    return d
+      .toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false
+      })
+      .replace(',', '')
+  }
+
   return (
-    <div>
-      <div className="btn btn-sm">Test</div>
+    <div className="w-2/3 flex flex-col gap-4">
+      <div className="flex justify-between rounded-2xl border-2 border-dashed border-yellow-500/10 bg-yellow-500/5 p-6">
+        <div className="flex flex-col gap-1">
+          <div className="badge badge-soft">
+            <Wallet className="w-4 h-4" />
+            Balance
+          </div>
+        </div>
+        <div className="text-4xl font-bold text-white">
+          {balance.toFixed(2)} $
+        </div>
+      </div>
     </div>
   )
 }

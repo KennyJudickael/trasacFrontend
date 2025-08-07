4d978a8c22d4d359a9d86feec486fc5e58ad0ce5
RAVOTINA Kenny Judickael <judickaelkenny@gmail.com>
2025-08-07 17:50:45 +0300

    toast notification

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,37 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import toast from 'react-hot-toast'
+import api from './api'
+
+type Transaction = {
+  id: string
+  text: string
+  amount: number
+  createdAt: string
+}
+
 export default function Home() {
-  return <div className="btn btn-sm">test</div>
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  const getTransactions = async () => {
+    try {
+      const res = await api.get<Transaction[]>('transactions/')
+      setTransactions(res.data)
+      toast.success('Transactions fetched successfully')
+    } catch (error) {
+      console.error('Error fetching transactions:', error)
+      toast.error('Failed to fetch transactions')
+    }
+  }
+
+  useEffect(() => {
+    getTransactions()
+  }, [])
+
+  return (
+    <div>
+      <div className="btn btn-sm">Test</div>
+    </div>
+  )
 }
